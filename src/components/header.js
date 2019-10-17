a16a58e4f7d0775f39d2576e05db90c201f1522a
Silvia <silvia@desktop-b99mo2o.learnet.se>
2019-10-17 14:05:29 +0200

    the menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,76 @@
 import React from 'react';
 
 
+export default class Header extends React.Component {
 
+  constructor() {
+    super();
 
-export default class Header extends React.Component{
-
-    // constructor(props){
-    //   super(props)
-    // }
-    render(){
-      return (
-        <>
-          <header className="header">
-            <div className="logo">
-            logga
-            </div>
-            <div className="menu">
-            menu
-            </div>
-          </header>
-        </>
-      )
+    this.state = {
+      showMenu: false,
     }
+    this.showMenu = this.showMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+
+  }
+  showMenu(event) {
+    event.preventDefault();
+    // cancela o evento se for cancelavel
+    this.setState({ showMenu: true }, () => {
+      //document.addEventListener('click', this.closeMenu);
+
+    });
+  }
+
+  closeMenu(event) {
+    if (!this.dropdownMenu.contains(event.target)) {
+
+
+      this.setState({ showMenu: false }, () => {
+        //document.addEventListener('click', this.closeMenu);
+      });
+    }
+  }
+  render() {
+    return (
+      <div>
+
+        <header className="header">
+
+          <div className="logo">
+            <div><a href="/">The logo</a></div>
+          </div>
+
+
+          <div className="menu1">
+            <button onClick={this.showMenu}>
+              Show menu</button>
+          </div>
+
+          {
+            this.state.menu,
+            this.state.showMenu
+              ? (
+
+                <div
+                  className="menu" ref={(element) => {
+                    this.dropdownMenu = element;
+                  }}
+                >
+
+                  <button>menu 1</button>
+                  <button>menu 2</button>
+                  <button>menu 3</button>
+                </div>
+              )
+              : (
+                null
+              )
+          }
+        </header>
+
+      </div>
+    );
+  }
 
 };
